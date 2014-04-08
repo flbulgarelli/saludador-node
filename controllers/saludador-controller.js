9b632322438d1d2b7c658a81e288fae5c6831ad5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 15:46:00 -0300

    Separando vista y controlador usando templates

--- controllers/saludador-controller.js
+++ controllers/saludador-controller.js
@@ -2,9 +2,9 @@ var saludador = require('../models/saludador');
 
 module.exports = {
   saludo: function(req, res) {
-    res.send('<html><head><title>Hola Express</title></head><body><h1>Página de Bienvenida</h1><p>' + saludador.saludar(req.query.nombre) + '</p></body></html>');
+    res.render('saludo', { mensaje: saludador.saludar(req.query.nombre) } );
   },
   despedida: function(req, res) {
-    res.send('<html><head><title>Hola Express</title></head><body><h1>Página de Despedida</h1><p>' + saludador.despedir(req.query.nombre) + '</p></body></html>');
+    res.render('despedida', { mensaje: saludador.despedir(req.query.nombre) } );
   }
 }
