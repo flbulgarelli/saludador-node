95e29835fdbf80391f6d98c8a9362de80cacc1fa
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 11:36:17 -0300

    Version basica de un servidor Hola Mundo

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http')
+
+http.createServer(function(req, res) { 
+  res.write("<html><head><title>Hola node</title></head><body>Hola Mundo!</body></html>"); 
+  res.end(); 
+}).listen(8080);
+
+console.log("Servidor iniciado")
