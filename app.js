18e4cede622602931327552d1ac21f1b852751ee
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 13:15:52 -0300

    Agregando nuevos saludos

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var saludador = require('./models/saludador');
 var app = express();
 app.use(app.router);
 
-app.get('/', function (req, res) {
-  res.send('<html><head><title>Hola Express</title></head><body>' + saludador.saludar(req.query.nombre) + '</body></html>');
+app.get('/saludo', function (req, res) {
+  res.send('<html><head><title>Hola Express</title></head><body><h1>Página de Bienvenida</h1><p>' + saludador.saludar(req.query.nombre) + '</p></body></html>');
 });
+app.get('/despedida', function (req, res) {
+  res.send('<html><head><title>Hola Express</title></head><body><h1>Página de Despedida</h1><p>' + saludador.despedir(req.query.nombre) + '</p></body></html>');
+});
+
 
 var server = app.listen(8080, function () {
   console.log('Servidor iniciado');
--- spec/saludador-spec.js
+++ spec/saludador-spec.js
@@ -1,7 +1,10 @@
 var saludador = require('../models/saludador');
 
 describe("Saludador", function() {
-  it("saluda a la persona dicendo hola", function() {
+  it("saluda a la persona dicendo 'hola'", function() {
     expect(saludador.saludar("Pepe")).toBe("Hola Pepe!");
+  });
+  it("despueda a la persona diciendo 'adios'", function() {
+    expect(saludador.despedir("Pepe")).toBe("Adios Pepe :(");
   });
 });
