82ff6ace9fadefc9319228a6930d85afca0c1dc6
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 18:38:06 -0300

    Agregando Home

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
+var home = require('../controllers/home-controller');
 var saludador = require('../controllers/saludador-controller');
 
 module.exports = function(app) {
+  app.get('/', home.index);
   app.get('/saludo', saludador.saludo);
   app.get('/despedida', saludador.despedida);
 }
