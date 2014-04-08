c6ee29dc161faed19156e5904a707bae7248d49b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 15:32:57 -0300

    Agregando dependencias de handlebars

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var routes =  require('./config/routes');
+var handlebars = require('express3-handlebars');
+var path = require('path');
 
 var app = express();
 app.use(app.router);
 
 routes(app);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars({}));
+app.set('view engine', 'handlebars');
+
 
 var server = app.listen(8080, function() {
   console.log('Servidor iniciado');
