0dc47df3180c30e30a4b51c827a68d1cc0631fc3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 13:25:09 -0300

    Extrayendo las rutas a su propio modulo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var routes =  require('./config/routes');
+
 var saludador = require('./controllers/saludador-controller');
 
 var app = express();
 app.use(app.router);
 
-app.get('/saludo', saludador.saludo);
-app.get('/despedida', saludador.despedida);
+routes(app);
 
 var server = app.listen(8080, function() {
   console.log('Servidor iniciado');
