31adc0808493663e53f9183e97a7971e968555d6
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 12:49:59 -0300

    Usando rutas de express

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
 var saludador = require('./models/saludador');
 
-http.createServer(function(req, res) { 
-  var query = url.parse(req.url, true).query;
-  res.write("<html><head><title>Hola node</title></head><body>"+saludador.saludar(query.nombre)+"</body></html>"); 
-  res.end(); 
-}).listen(8080);
+var app = express();
+app.use(app.router);
 
-console.log("Servidor iniciado")
+app.get('/', function (req, res) {
+  res.send('<html><head><title>Hola Express</title></head><body>' + saludador.saludar(req.query.nombre) + '</body></html>');
+});
+
+var server = app.listen(8080, function () {
+  console.log('Servidor iniciado');
+});
