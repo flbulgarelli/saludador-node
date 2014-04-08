c56f7ae195bfbd0633565c9944ffd357bc05823e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-08 11:43:39 -0300

    Extrayendo modelo

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var url = require('url');
 
+var saludador = {
+  saludar: function(nombre) {
+   return "Hola " + nombre + "!";   
+  }
+}
+
 http.createServer(function(req, res) { 
   var query = url.parse(req.url, true).query;
-  res.write("<html><head><title>Hola node</title></head><body>Hola "+query.nombre+"!</body></html>"); 
+  res.write("<html><head><title>Hola node</title></head><body>"+saludador.saludar(query.nombre)+"</body></html>"); 
   res.end(); 
 }).listen(8080);
 
